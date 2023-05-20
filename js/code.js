1c676983c0766549f14c78dddbc517d347ef2d99
codjoelmayer <tmjoelm4@gmail.com>
2023-05-20 05:37:07 +0200

    Awesoe

--- js/code.js
+++ js/code.js
@@ -1,7 +1,11 @@
+// First operand
+let firstOp = 0;
 // Output
 let output = document.querySelector('#output');
 // Regular expression
 let regExp = new RegExp(/^(\d{0,})*(\d{0,})?$/);
+// Operator
+let operator = "";
 /*
 \d
 [0-9]
@@ -15,34 +19,51 @@ let numbOps = document.querySelectorAll('.numbOps');
 // Equals
 let btnEqual = document.querySelector('#btnEquals');
 let cnt = 0;
+// AC
+function clear() {
+    output.value = "";
+}
+document.querySelector('#btnAC').addEventListener('click', clear)
+// Backspace
+document.querySelector('#btnDel').addEventListener('click', ()=>{
+    output.value = output.value.slice(0, -1);
+})
 // Event listener
 numbOps.forEach((btnNumbOps, i)=> {
     btnNumbOps.addEventListener('click', ()=> {
         if(regExp.test(output.value))
             output.value += btnNumbOps.innerText;
-
-        // if((output.value.length > 1) && (cnt == 1)) {
-        //     output.value = "";
-        // }else {
-        //     cnt = 0;
-        // }
+    })
+})
+// Operator
+document.querySelectorAll('.operators')
+.forEach((ops)=>{
+    ops.addEventListener('click', (e)=>{
+        firstOp = output.value?.length ? output.value.slice(0, -1)
+        : 0;
+        operator = e.target.innerText;
+        clear();
     })
 })
 // Equal
 btnEqual.addEventListener('click', ()=> {
     try {
-        output.value = eval(output.value); 
-        cnt++;
+        switch(operator) {
+            case '+':
+                output.value = eval(`${firstOp} + ${output.value}`).toFixed(2); 
+            break;
+            case '/':
+                output.value = eval(`${firstOp} / ${output.value}`).toFixed(2); 
+            break;
+            case '*':
+                output.value = eval(`${firstOp} * ${output.value}`).toFixed(2); 
+            break;
+            case '-':
+                output.value = eval(`${firstOp} - ${output.value}`).toFixed(2); 
+            break;
+        }
     }catch(e) {
         alert(`An error occurred, please try again: ${e.message}`);
     }
 
-})
-// AC
-document.querySelector('#btnAC').addEventListener('click', ()=>{
-    output.value = "";
-})
-// Backspace
-document.querySelector('#btnDel').addEventListener('click', ()=>{
-    output.value = output.value.slice(0, -1);
 })
